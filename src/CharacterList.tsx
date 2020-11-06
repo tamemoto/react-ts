1477968c9c06b7c4f35d827160b90926df33edd9
tamemotoyusuke <tamemotoyou@gmail.com>
2020-11-06 14:55:46 +0900

    update

--- src/CharacterList.tsx
+++ src/CharacterList.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable react/prefer-stateless-function */
 // eslint-disable-next-line no-use-before-define
-import React, { Component, ReactElement } from 'react';
+import React, { FC } from 'react';
 import { Header, Icon, Item } from 'semantic-ui-react';
 
 export type Character = {
@@ -15,30 +14,28 @@ type Props = {
   characters: Character[];
 };
 
-class CharacterList extends Component<Props> {
-  render(): ReactElement {
-    const { school, characters } = this.props;
+const CharacterList: FC<Props> = (props) => {
+  const { school, characters } = props;
 
-    return (
-      <>
-        <Header as="h2">{school}</Header>
-        <Item.Group>
-          {characters.map((character) => {
-            return (
-              <Item key={character.id}>
-                <Icon name="user circle" size="huge" />
-                <Item.Content>
-                  <Item.Header>{character.name}</Item.Header>
-                  <Item.Meta>{character.grade}</Item.Meta>
-                  <Item.Meta>{character.height ?? '???'}cm</Item.Meta>
-                </Item.Content>
-              </Item>
-            );
-          })}
-        </Item.Group>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Header as="h2">{school}</Header>
+      <Item.Group>
+        {characters.map((character) => {
+          return (
+            <Item key={character.id}>
+              <Icon name="user circle" size="huge" />
+              <Item.Content>
+                <Item.Header>{character.name}</Item.Header>
+                <Item.Meta>{character.grade}</Item.Meta>
+                <Item.Meta>{character.height ?? '???'}cm</Item.Meta>
+              </Item.Content>
+            </Item>
+          );
+        })}
+      </Item.Group>
+    </>
+  );
+};
 
 export default CharacterList;
---
+++ src/data/characters.ts
@@ -0,0 +1,35 @@
+import { Character } from '../CharacterList';
+
+const characters: Character[] = [
+  {
+    id: 1,
+    name: '桜木花道',
+    grade: 1,
+    height: 189.2,
+  },
+  {
+    id: 2,
+    name: '流川 楓',
+    grade: 1,
+    height: 187,
+  },
+  {
+    id: 3,
+    name: '宮城リョータ',
+    grade: 2,
+    height: 168,
+  },
+  {
+    id: 4,
+    name: '三井 寿',
+    grade: 3,
+  },
+  {
+    id: 5,
+    name: '赤木剛憲',
+    grade: 3,
+    height: 197,
+  },
+];
+
+export default characters;
